bab3a7a39ed5b53431ddfa267cc583f666c7d0af
Muhammad Aimal Siddique <aimaldev5@gmail.com>
2025-06-05 15:58:03 +0500

    Immersive route update for immersive2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Chat from "./pages/chat";
 import OnlyChat from "./pages/only-chat";
 import StreamImpressive from "./pages/StreamImpressive";
+import Immersive from "./pages/Immersive/index";
 import Immersive2 from "./pages/Immersive/Immersive2";
 import "./App.css";
 import "@draft-js-plugins/emoji/lib/plugin.css";
@@ -18,7 +19,8 @@ function App() {
           path="/stream-impressive"
           element={<StreamImpressive />}
         />
-        <Route strict path="/immersive" element={<Immersive2 />} />
+        <Route strict path="/immersive" element={<Immersive />} />
+        <Route strict path="/immersive2" element={<Immersive2 />} />
       </Routes>
     </BrowserRouter>
   );
