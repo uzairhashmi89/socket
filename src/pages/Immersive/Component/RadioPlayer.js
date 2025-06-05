0913a4df41a5c191ae312bfff82157e12a9b48dd
Muhammad Aimal Siddique <aimaldev5@gmail.com>
2025-06-05 19:36:24 +0500

    Immersive video stretch issue resolved

--- src/pages/Immersive/Component/RadioPlayer.js
+++ src/pages/Immersive/Component/RadioPlayer.js
@@ -4,30 +4,40 @@ import ReactPlayer from 'react-player';
 
 const RadioPlayer = ({ url, width }) => {
   return (
-    <div  style={{
-        width: width,
-        height: '100vh',
-        backgroundColor: '#000',
-        overflow: 'hidden',
-        margin: 0,
-        padding: 0,
-      }}
+    <div style={{
+      width: width,
+      height: '100vh',
+      backgroundColor: '#000',
+      overflow: 'hidden',
+      margin: 0,
+      padding: 0,
+    }}
       className='video-player'
-      >
+    >
       <ReactPlayer
         width="100%"
         height="100%"
+        style={{ objectFit: 'contain' }}
         url={url}
         playing
         autoPlay={true}
         muted={true}
         controls={false}
         config={{
-            file: {
-              forceHLS: true,
+          file: {
+            forceHLS: true,
+            attributes: {
+              style: {
+                width: '100%',
+                height: '100%',
+                aspectRatio: "16/9", // or "auto" if ReactPlayer handles it well
+                objectFit: "cover", // optional: to stretch and remove black bars
+              },
             },
-          }}
-        />
+
+          },
+        }}
+      />
     </div>
   );
 };
