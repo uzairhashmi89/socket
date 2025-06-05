b89d5598996760b91f9f89d88d1051a457d88b54
Muhammad Aimal Siddique <aimaldev5@gmail.com>
2025-06-05 15:48:03 +0500

    Immersive Done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Chat from "./pages/chat";
 import OnlyChat from "./pages/only-chat";
 import StreamImpressive from "./pages/StreamImpressive";
-import Immersive from "./pages/Immersive";
+import Immersive from "./pages/Immersive/new";
 import "./App.css";
 import "@draft-js-plugins/emoji/lib/plugin.css";
 import ChatNew from "./pages/chat/new";
